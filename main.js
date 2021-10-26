d697de1462120909e389edfdab3abf358a49118c
Chiraag Jain <78903401+ChiraagJain@users.noreply.github.com>
2021-10-26 18:18:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,102 @@
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext("2d"); 
+
+car_width = 150;
+car_height = 120;
+
+background_image = "parkingLot.jpg";
+greencar_image = "car2.png";
+
+car_x = 500;
+car_y = 10;
+
+function add() {
+	background_imgTag = new Image(); 
+	background_imgTag.onload = uploadBackground; 
+	background_imgTag.src = background_image;  
+
+	rover_imgTag = new Image(); 
+	rover_imgTag.onload = uploadgreencar; 
+	rover_imgTag.src = greencar_image;
+}
+
+function uploadBackground() {
+ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
+
+}
+
+function uploadgreencar() {
+	ctx.drawImage(greencar_imgTag, 0, 0, car.width, car.height);
+
+	
+}
+
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+		if(keyPressed == '38')
+		{
+			up();
+			console.log("up");
+		}
+	
+		if(keyPressed == '40')
+		{
+			down();
+			console.log("down");
+		}
+		
+		if(keyPressed == '37')
+		{
+			left();
+			console.log("left");
+		}
+	
+		if(keyPressed == '39')
+		{
+			right();
+			console.log("right");
+		}
+		
+		
+}
+
+function up()
+{
+	if (car_y >= 550){
+		car_y = car_y - 10;
+		uploadgreencar();
+		uploadBackground();
+	}
+}
+
+function down()
+{
+	if (car_y >= 0) {
+		car_y = car_y + 10;
+		uploadgreencar();
+		uploadBackground();
+	}
+}
+
+function left()
+{
+	if (car_x >= 0){
+		car_x = car_x + 10;
+		uploadgreencar();
+		uploadBackground();
+	}
+}
+
+function right()
+{
+	if (car_x >=750 ) {
+		car_x = car_x - 10;
+		uploadgreencar();
+		uploadBackground();
+	}
+}
